e8b808afb2469272187f251beb4331da6b6986b2
Viktoria <aliceinapple99@gmail.com>
2023-07-01 12:05:45 +0300

    feat: implement mocking Node.js API

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,52 +1,137 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     jest.useFakeTimers();
   });
 
-  afterAll(() => {
+  afterEach(() => {
+    jest.clearAllTimers();
     jest.useRealTimers();
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 1000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(timeout);
+
+    expect(callback).toBeCalled();
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 1000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(timeout - 1);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(1);
+
+    expect(callback).toBeCalled();
   });
 });
 
 describe('doStuffByInterval', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     jest.useFakeTimers();
   });
 
-  afterAll(() => {
+  afterEach(() => {
+    jest.clearAllTimers();
     jest.useRealTimers();
   });
 
-  test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+  test('should set interval with provided callback and interval', () => {
+    const callback = jest.fn();
+    const interval = 1000;
+
+    doStuffByInterval(callback, interval);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(interval);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersByTime(interval);
+
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const interval = 1000;
+
+    doStuffByInterval(callback, interval);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(interval - 1);
+
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(1);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersByTime(interval);
+
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    const pathToFile = 'file.txt';
+    const joinSpy = jest.spyOn(require('path'), 'join');
+
+    await readFileAsynchronously(pathToFile);
+
+    expect(joinSpy).toHaveBeenCalledTimes(1);
+    expect(joinSpy).toHaveBeenCalledWith(__dirname, pathToFile);
+
+    joinSpy.mockRestore();
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    const pathToFile = 'nonexistent.txt';
+    const existsSyncMock = jest
+      .spyOn(require('fs'), 'existsSync')
+      .mockReturnValue(false);
+
+    const result = await readFileAsynchronously(pathToFile);
+
+    expect(result).toBeNull();
+
+    existsSyncMock.mockRestore();
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const pathToFile = 'file.txt';
+    const fileContent = 'Hello, world!';
+    const existsSyncMock = jest
+      .spyOn(require('fs'), 'existsSync')
+      .mockReturnValue(true);
+    const readFileMock = jest
+      .spyOn(require('fs/promises'), 'readFile')
+      .mockResolvedValue(Buffer.from(fileContent));
+
+    const result = await readFileAsynchronously(pathToFile);
+
+    expect(result).toBe(fileContent);
+
+    existsSyncMock.mockRestore();
+    readFileMock.mockRestore();
   });
 });
